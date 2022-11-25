21e8de6cfa25751285cb0716d85f3f089e5edc12
Vishesh Gupta <66796715+vishesh13byte@users.noreply.github.com>
2022-11-26 04:09:18 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t-- > 0){
+	    ll n,x,y,z,k,m;
+	    cin >> n >> m >> k >> x;
+	    //string s;
+	    //cin >> s;
+	    // string str;
+	    // vector<int> v(n);
+	    // for(int i = 0; i < n; i++){}
+	    // x = n;
+	    // y = x;
+	    // z = y;
+	    // k = 0;
+	    // x = y;
+	    // y = z;
+	    //cout << k << "\n";
+	}
+	return 0;
+}
