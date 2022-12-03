33e0847159fccedbe4a281a58c6e3740bb6544aa
Vishesh Gupta <66796715+vishesh13byte@users.noreply.github.com>
2022-12-03 16:42:39 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,28 +6,20 @@ typedef long long int ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
 
-#define fori(i,a,b) for (int i = a; i <= b; i++)
-#define ford(i,a,b) for (int i = a; i >= b; i--)
+#define fori(i,a,b) for(int i = a; i <= b; i++)
+#define ford(i,a,b) for(int i = a; i >= b; i--)
+#define endl "\n"
 
 int main() {
-	// your code goes here
 	int t;
 	cin >> t;
 	while(t-- > 0){
-	    ll n,x,y,z,k,m;
-	    cin >> n >> m >> k >> x;
-	    //string s;
-	    //cin >> s;
-	    // string str;
-	    // vi v(n);
-	    // for(int i = 0; i < n; i++){}
-	    // x = n;
-	    // y = x;
-	    // z = y;
-	    // k = 0;
-	    // x = y;
-	    // y = z;
-	    //cout << k << "\n";
+	    ll n,x,y,z,k,m,i;
+	    string s; 
+	    cin >> n >> m >> k >> x >> s;
+	    vi arr(n);
+	    fori(i,0,n-1);
+	    //cout << k << endl;
 	}
 	return 0;
 }
